8f757afa141362987a7259162b6ce6016e6f355b
fire-irror <firenoah.m@gmail.com>
2023-08-08 19:35:26 +0900

    ViewFlipper 예제 완성

--- app/src/main/java/kr/hs/emirim/viewflippertest/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/viewflippertest/MainActivity.kt
@@ -2,10 +2,24 @@ package kr.hs.emirim.viewflippertest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ViewFlipper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+        var btnPre = findViewById<Button>(R.id.btnPre)
+        var btnNext = findViewById<Button>(R.id.btnNext)
+        var flipper = findViewById<ViewFlipper>(R.id.flipper1)
+
+        btnPre.setOnClickListener{
+            flipper.showPrevious()
+        }
+
+        btnNext.setOnClickListener {
+            flipper.showNext()
+        }
+        }
 }
